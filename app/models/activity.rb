649bbc06ebac8ac3593010508cf4894f2b89e7d8
Gabor Lengyel <glengyel@prodexity.com>
2018-01-21 17:40:25 +0100

    added more complex associations around performed and not performed activities by org, and orgs performing or not by activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,7 +6,20 @@ class Activity < ApplicationRecord
   has_one :domain, through: :practice
 
   has_many :activity_orgs, dependent: :destroy
-  has_many :orgs, through: :activity_orgs
+  has_many :orgs_answered, # Orgs that provided an answer
+           through: :activity_orgs,
+           class_name: "Org",
+           source: :org
+  has_many :orgs_perform, # Orgs that perform the activity
+           -> { where('activity_orgs.answer = ?', 1) },
+           class_name: "Org",
+           through: :activity_orgs,
+           source: :org
+  has_many :orgs_not_perform, # Orgs that do not perform the activity
+           -> { where('activity_orgs.answer = ?', 0) },
+           class_name: "Org",
+           through: :activity_orgs,
+           source: :org
 
   has_many :activity_verticals, dependent: :destroy
   has_many :verticals, through: :activity_verticals
--- app/models/org.rb
+++ app/models/org.rb
@@ -3,5 +3,18 @@ class Org < ApplicationRecord
   has_many :users, through: :org_users
 
   has_many :activity_orgs, dependent: :destroy
-  has_many :activities, through: :activity_orgs
+  has_many :activities_answered, # Activities for which the Org provided an answer
+           through: :activity_orgs,
+           class_name: "Activity",
+           source: :activity
+  has_many :activities_performed, # Activities that the Org performs
+           -> { where('activity_orgs.answer = ?', 1) },
+           through: :activity_orgs,
+           class_name: "Activity",
+           source: :activity
+  has_many :activities_not_performed, # Activities that the Org does not perform
+           -> { where('activity_orgs.answer = ?', 0) },
+           through: :activity_orgs,
+           class_name: "Activity",
+           source: :activity
 end
