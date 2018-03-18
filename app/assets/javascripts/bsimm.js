2544cad3fba5408d172363e910f184360573757c
Gabor Lengyel <glengyel@prodexity.com>
2018-03-18 03:29:04 +0100

    added activity descriptions and buttons (introduces bug, ajax partial html and knockout don't fit)

--- app/assets/javascripts/bsimm.js
+++ app/assets/javascripts/bsimm.js
@@ -5,13 +5,30 @@ function BsimmViewModel() {
 
     self.showPage = function(vm, ev) {
       var ppath = ev.target.dataset.practicepath;
-      $('#bsimmcontent').load(ppath);
+      var bsimmcontent = $('#bsimmcontent');
+      bsimmcontent.load(ppath);
+      /*
+      // this doesn't help:
+      ko.cleanNode(bsimmcontent[0]);
+      ko.applyBindings(vm, bsimmcontent[0]);
+      */
       history.replaceState(null, null, ppath);
       self.currentPage(ppath);
     }
 
+    self.setYesNoBtnColor = function(vm, ev) {
+      // this is a hack because there is no viewmodel for answers as answer pages are partial htmls loaded via ajax
+      var btn = $(ev.target);
+      btn.removeClass('btn-default');
+      var inp = $("input[type='radio']", btn);
+      $('div.btn', btn.parent()).removeClass('btn-success').removeClass('btn-danger').addClass('btn-default');
+      if (inp.val() == 'yes') btn.addClass('btn-success');
+      if (inp.val() == 'no') btn.addClass('btn-danger');
+      return true;
+    };
 };
 
 $(function() {
   ko.applyBindings(new BsimmViewModel());
 });
+
