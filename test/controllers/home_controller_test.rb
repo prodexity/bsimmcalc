12cadf4c3ce580d228798c08f6c663b8264b26e5
Gabor Lengyel <glengyel@prodexity.com>
2018-03-22 13:53:37 +0100

    fixed test of index page http return value

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get home_index_url
-    assert_response :redirect
+    # assert_response :redirect
+    assert_response :success
   end
 
 end
