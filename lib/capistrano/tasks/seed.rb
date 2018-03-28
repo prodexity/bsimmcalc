6e3c9f24fd6810baa272b4b16d1e5ff6fe68bde6
Gabor Lengyel <glengyel@prodexity.com>
2018-03-28 09:55:51 +0200

    fixed typo in capistrano seed.rb

--- lib/capistrano/tasks/seed.rb
+++ lib/capistrano/tasks/seed.rb
@@ -2,7 +2,7 @@ namespace :deploy do
   task :seed do
     on primary fetch(:migration_role) do
       within release_path do
-        with rails_env: fetch(:rails_env)  do
+        with rails_env: fetch(:rails_env) do
           execute :rake, 'db:seed'
         end
       end
