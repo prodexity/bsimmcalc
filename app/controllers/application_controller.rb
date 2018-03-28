be444ea984fd9cbe5544e86c41679dc8dd52a5d5
Gabor Lengyel <glengyel@prodexity.com>
2018-03-28 15:09:59 +0200

    added revision to footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_bsimm_tree
+  before_action :set_bsimm_tree, :revision
 
   def set_bsimm_tree
     @domains = Domain.all
+  end
+
+  def revision
+    @revision = File.read(Rails.root + 'REVISION')
   end
 end
